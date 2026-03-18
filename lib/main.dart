37f8f89318d028ba390be94f52b4cacee6a836ca
McDo <mrjenald@gmail.com>
2026-03-18 19:53:36 +0800

    FINAL THESIS NEW

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
-import 'starting_page.dart';
+import 'package:flutter/services.dart';
+import 'app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const HuniApp());
-}
-
-class HuniApp extends StatelessWidget {
-  const HuniApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HUNI',
-      theme: ThemeData(
-        fontFamily: 'Arial',
-        scaffoldBackgroundColor: Colors.black,
-      ),
-      home: const StartingPage(),
-    );
-  }
 }
